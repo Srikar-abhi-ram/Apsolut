d6f224646acb05c33bd409db409d1577c13aec11
Srikar-abhi-ram <66876519+Srikar-abhi-ram@users.noreply.github.com>
2021-11-07 19:40:04 +0530

    seconf commit

---
+++ lib/screens/ariba_subscreens/editariba.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class Editariba extends StatefulWidget {
+  @override
+  _EditaribaState createState() => _EditaribaState();
+}
+
+class _EditaribaState extends State<Editariba> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Text("This is edit page"),
+    );
+  }
+}
