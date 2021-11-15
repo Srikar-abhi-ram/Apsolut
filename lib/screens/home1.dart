24572f4f35b89d17b19d1d033e96e93dff237eb8
Srikar-abhi-ram <66876519+Srikar-abhi-ram@users.noreply.github.com>
2021-11-15 12:06:47 +0530

    Update home1.dart

--- lib/screens/home1.dart
+++ lib/screens/home1.dart
@@ -26,80 +26,153 @@ class _Home1State extends State<Home1> {
             centerTitle: true,
             brightness: Brightness.dark,
           ),
-      body:Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(''),
-        
-          fit: BoxFit.cover
-          )),
-        child:Padding(
-          padding: const EdgeInsets.fromLTRB(0, 90, 0, 300.0),
-
-          child: LineChart(LineChartData(minX: 0,maxX: 4,minY: 0,maxY: 7,
-          titlesData: LineTitles.getTitleData(),
-          gridData: FlGridData(show: true,getDrawingHorizontalLine: (value){
-            return FlLine(color: Colors.black,strokeWidth: 1);
-          },
-          drawVerticalLine: true,
-          getDrawingVerticalLine: (value) {
-              return FlLine(
-                color: Colors.black,
-                strokeWidth: 1,
-              );
-            },
-          ),
-           borderData: FlBorderData(
-            show: true,
-            border: Border.all(color: const Color(0xff37434d), width: 3),
-            
-          ),
-   lineBarsData: [
-     LineChartBarData(
-       spots: [
-         FlSpot(1,2 ),
-         FlSpot(2,2 ),
-         FlSpot(3,2 ),
-       ],
-       isCurved: true,
-        barWidth: 3,
-        dotData: FlDotData(show: false),
-        
-       ),
-     LineChartBarData(
-       spots: [
-          FlSpot(1,0 ),
-         FlSpot(2,5),
-         FlSpot(3,6 ),
-       ],
-       isCurved: true,
-        barWidth: 3,
-        dotData: FlDotData(show: false),
-        colors: [
-          Colors.red
-        ]
-        
-
-       ),
-       LineChartBarData(
-           spots: [  
-           FlSpot(1,1),
-            FlSpot(2,1),
-            FlSpot(3,1),
-       ],
+      body:
        
-       isCurved: true,
-        barWidth: 3,
-        dotData: FlDotData(show: false),
-        colors: [
-          Colors.amber
-        ]
-       )
-   ]
-   )),
-        
-   ),
-      )
-  );
+        Stack(
+          children: [
+            Padding(              
+          padding: const EdgeInsets.fromLTRB(0, 90, 0, 100.0),
+              child: Card(
+                child: LineChart(
+                  
+                  LineChartData(minX: 0,maxX: 4,minY: 0,maxY: 7,
+                titlesData: LineTitles.getTitleData(),
+                gridData: FlGridData(show: true,getDrawingHorizontalLine: (value){
+                  return FlLine(color: Colors.black,strokeWidth: 1);
+                },
+                drawVerticalLine: true,
+                getDrawingVerticalLine: (value) {
+                    return FlLine(
+                      color: Colors.black,
+                      strokeWidth: 1,
+                    );
+                  },
+                ),
+                 borderData: FlBorderData(
+                  show: true,
+                  border: Border.all(color: const Color(0xff37434d), width: 3),
+                  
+                ),
+                         lineBarsData: [
+                 LineChartBarData(
+                   spots: [
+                         FlSpot(1,1.8 ),
+                         FlSpot(2,1.8 ),
+                         FlSpot(3,1.8 ),
+                   ],
+                   isCurved: true,
+                        barWidth: 3,
+                        dotData: FlDotData(show: true),
+                        
+                   ),
+                 LineChartBarData(
+                   spots: [
+                        FlSpot(1,2.7 ),
+                         FlSpot(2,2.7),
+                         FlSpot(3,2.7 ),
+                   ],
+                   isCurved: true,
+                        barWidth: 3,
+                        dotData: FlDotData(show: true),
+                        colors: [
+                Colors.red
+                        ]
+                        
+                      
+                   ),
+                   LineChartBarData(
+                 spots: [  
+                 FlSpot(1,1),
+                  FlSpot(2,1),
+                  FlSpot(3,1),
+                   ],
+                   
+                   isCurved: true,
+                        barWidth: 3,
+                        dotData: FlDotData(show: true),
+                        colors: [
+                Colors.amber
+                        ]
+                   )
+                         ]
+                         )),
+              ),
+            ),
+       Padding(
+         padding: const EdgeInsets.fromLTRB(50,0, 0, 50),
+         child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(children: [
+                    Row(
+      children: <Widget>[
+        Container(
+          width: 13,
+          height: 13,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.red,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          "Capacity ",
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, ),
+        )
+      ],
+                  ),
+                               Row(
+      children: <Widget>[
+        Container(
+          width: 13,
+          height: 13,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.blue,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          " BreakEven ",
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold,  ),
+        )
+      ],
+                  ),
+                               Row(
+      children: <Widget>[
+        Container(
+          width: 13,
+          height: 13,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.amber,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          "Total Pipe  ",
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, ),
+        )
+      ],
+                  ),
+                  //the row widget inside the row widget ends here      
+                  ],)          
+                   ],
+              ),
+       ),
+          ]
+          
+          ),
+      );
   
   }
 }
